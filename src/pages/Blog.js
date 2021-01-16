3c7892823276d36b375300bbba1415ac68a4fb12
Anton Bertilsson <bertilsson.anton@gmail.com>
2021-01-16 10:08:46 +0100

    add placeholder content on blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const Blog = () => {
   return (
-    <div className='main-wrapper'>
+    <div className="main-wrapper">
       <h1>Blog</h1>
-    </div>
-  )
-}
 
-export default Blog
+      <a href="#">Journal: My Goals For 2021</a>
+
+      <a href="#">Review: M1 Macbook Pro 13"</a>
+
+      <a href="#">Tutorial: Adding Dark Mode</a>
+
+      <a href="#">Tutorial: Adding Language Support</a>
+
+      <a href="#">Tutorial: Simple Contact Form Validation</a>
+
+      <a href="#">Tutorial: Hamburger Animations</a>
+
+      <a href="#">Tutorial: Responsive Hamburger Menu</a>
+
+      <a href="#">Journal: My Learning Journey</a>
+    </div>
+  );
+};
+
+export default Blog;
