3e14f8470eebd3c78144ffc1b4463508f6569165
Anton Bertilsson <bertilsson.anton@gmail.com>
2021-01-17 19:11:15 +0100

    add content to about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import Text, { textTypes } from '../components/Text';
+import copyKeys from '../utils/CopyService';
+import '../styles/layouts/about.scss';
+import lovem1 from '../assets/lovem1.jpg';
+import aboutImg2 from '../assets/aboutImg2.jpg';
 
 const About = () => {
   return (
     <div className="main-wrapper">
-      <h1>About</h1>
+      <div className="about-content">
+        <div className="about-body">
+          <Text
+            className="about-header clr-text-primary"
+            copyKey={copyKeys.AboutHeaderAbout}
+          />
+          <img src={lovem1} alt="Anton" className="about-img" />
+          <Text className="about-paragraph" copyKey={copyKeys.AboutParaOne} />
+          <Text className="about-paragraph" copyKey={copyKeys.AboutParaTwo} />
+
+          <Text
+            className="about-header clr-text-primary"
+            copyKey={copyKeys.AboutHeaderStrengths}
+          />
+          <Text className="about-paragraph" copyKey={copyKeys.AboutParaThree} />
+          <Text className="about-paragraph" copyKey={copyKeys.AboutParaFour} />
+
+          <Text
+            className="about-header clr-text-primary"
+            copyKey={copyKeys.AboutHeaderInterests}
+          />
+          <img src={aboutImg2} alt="Anton" className="about-img" />
+          <Text className="about-paragraph" copyKey={copyKeys.AboutParaFive} />
+          <Text className="about-paragraph" copyKey={copyKeys.AboutParaSix} />
+        </div>
+      </div>
     </div>
   );
 };
--- src/utils/CopyService.js
+++ src/utils/CopyService.js
@@ -14,6 +14,18 @@ const copyKeys = {
   HomeHeroHeader: 'hero.header',
   HomeHeroPresentation: 'hero.presentation',
 
+  /* ABOUT */
+  AboutHeaderAbout: 'about.header.about',
+  AboutHeaderStrengths: 'about.header.strengths',
+  AboutHeaderInterests: 'about.header.interests',
+
+  AboutParaOne: 'about.para.one',
+  AboutParaTwo: 'about.para.two',
+  AboutParaThree: 'about.para.three',
+  AboutParaFour: 'about.para.four',
+  AboutParaFive: 'about.para.five',
+  AboutParaSix: 'about.para.six',
+
   /* CONTACT > CONTACT FORM */
   ContactFormH1: 'contact.form.h1',
   ContactFormName: 'contact.form.name',
@@ -41,7 +53,7 @@ export const copy = [
   {
     key: copyKeys.NavAbout,
     en: 'About Me',
-    sv: 'Om Anton',
+    sv: 'Om mig',
   },
   {
     key: copyKeys.NavBlog,
@@ -71,6 +83,67 @@ export const copy = [
       "I'm a budding frontend developer from Stockholm, Sweden, with a passion for learning new things and creating cool stuff.",
     sv:
       'Jag är en blivande frontend utvecklare från Stockholm som älskar att lära mig nya saker och skapa spännande prylar.',
+  },
+
+  /* ABOUT > Headers */
+  {
+    key: copyKeys.AboutHeaderAbout,
+    en: 'About Me.',
+    sv: 'Om mig.',
+  },
+  {
+    key: copyKeys.AboutHeaderStrengths,
+    en: 'Strengths.',
+    sv: 'Styrkor.',
+  },
+  {
+    key: copyKeys.AboutHeaderInterests,
+    en: 'Interests.',
+    sv: 'Intressen.',
+  },
+
+  /* ABOUT > Paragraphs */
+  {
+    key: copyKeys.AboutParaOne,
+    en:
+      "I'm a budding frontend developer with a great interest in UI/UX design, who loves to code and build interactive, responsive websites with HTML, Sass and JavaScript in React.",
+    sv:
+      'Jag är en blivande frontend utvecklare med ett stort intresse för UI/UX-design som älskar att koda och bygga interaktiva, responsiva webbplatser med HTML, Sass och JavaScript i React.',
+  },
+  {
+    key: copyKeys.AboutParaTwo,
+    en:
+      "In the summer of 2020 I started to teach myself about programming and web development. Since then I have learnt how to code in JavaScript, HTML, CSS, Sass and React, as well as version control using Git. I have mainly learnt through resources such as Harvard's CS50, freeCodeCamp and TheOdinProject, as well as a myriad of personal and collaborative projects.",
+    sv:
+      'Under sommaren år 2020 började jag lära mig om programmering och webbutveckling. Sedan dess har jag lärt mig koda i JavaScript, HTML, CSS, Sass och React, samt versionskontrollera med Git. Jag har främst lärt mig genom resurser så som Harvards CS50, freeCodeCamp och TheOdinProject, samt en myriad av egna och kollaborativa projekt.',
+  },
+  {
+    key: copyKeys.AboutParaThree,
+    en:
+      'Creative, goal oriented and detail focused are the three words that best describe me. I find great joy in finding creative solutions to problems as well as in expressing myself creatively.',
+    sv:
+      'Kreativ, målmedveten och detaljorienterad är de tre ord som beskriver mig allra bäst. Jag finner stor glädje i att hitta kreativa lösningar på problem liksom i att uttrycka mig kreativt.',
+  },
+  {
+    key: copyKeys.AboutParaFour,
+    en:
+      "To be able to commit myself to a project is both motivating and stimulating for me and I always strive to finish my projects on time. I'm also keenly aware of details and the role they play with regards to the presentation of a project.",
+    sv:
+      'Att kunna engagera mig i ett projekt finner jag både motiverande och stimulerande och jag gör alltid mitt yttersta för att göra klart ett projekt i tid. Jag är även mån om detaljer och den viktiga roll de spelar vad gäller ett projekts presentation.',
+  },
+  {
+    key: copyKeys.AboutParaFive,
+    en:
+      'Besides UI/UX design and web development I find a lot of joy in learning about psychology, history and natural science. My ambition is to contribute to making the Internet as user friendly and accessible as possible for all people across the world as I feel that education and personal growth should be global human rights.',
+    sv:
+      'Utöver UI/UX-design och webbutveckling finner jag även glädje i att lära mig om psykologi, historia och naturvetenskap. Min ambition är att bidra till att göra Internet så användarvänligt och tilllgängligt som möjligt för alla människor världen över då jag anser att utbildning och personlig utveckling borde vara globala mänskliga rättigheter.',
+  },
+  {
+    key: copyKeys.AboutParaSix,
+    en:
+      "When I'm not coding I like to spend time with my friends and family, with my guitar or out in nature.",
+    sv:
+      'När jag inte kodar ägnar jag gärna tid med mina vänner och familj, med min gitarr eller ute naturen.',
   },
 
   /* CONTACT > FORM */
